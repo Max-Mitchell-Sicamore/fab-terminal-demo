b64bade0fdd5fafc7675257de59d488f706d5979
Colin Suckow <csuckow@sicamoresemi.com>
2020-03-05 10:34:54 -0800

    Poll server for updates

--- equipment_lights_test.py
+++ equipment_lights_test.py
@@ -1,4 +1,7 @@
 from equipment_light_bar_display import EquipmentLightBarDisplay
+import time
 
-equip_lightbar = EquipmentLightBarDisplay(5)
-equip_lightbar.display_equip_status(0,"E0414")
+while True:
+	equip_lightbar = EquipmentLightBarDisplay(5)
+	equip_lightbar.display_equip_status(0,"E0414")
+	time.sleep(1)

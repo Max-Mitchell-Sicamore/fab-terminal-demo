23e7d0b88c901b77c55313fb061936088bd77125
Max Mitchell <mmitchell@sicamoresemi.com>
2020-03-05 11:52:12 -0800

    revert to one segment

--- equipment_lights_test.py
+++ equipment_lights_test.py
@@ -6,8 +6,8 @@ import time
 while True:
     equip_lightbar = EquipmentLightBarDisplay(5)
     equip_lightbar.display_equip_status(0,"E0414")
-    equip_lightbar.display_equip_status(1,"E0341")
-    equip_lightbar.display_equip_status(2,"E0333")
-    equip_lightbar.display_equip_status(3,"E0413")
-    equip_lightbar.display_equip_status(4,"E0412")
+    # equip_lightbar.display_equip_status(1,"E0341")
+    # equip_lightbar.display_equip_status(2,"E0333")
+    # equip_lightbar.display_equip_status(3,"E0413")
+    # equip_lightbar.display_equip_status(4,"E0412")
     time.sleep(1)

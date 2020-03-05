336a6cd767d7e2309809a5e6c6857c55ee7fb027
Max Mitchell <mmitchell@sicamoresemi.com>
2020-03-05 11:38:11 -0800

    add mulitpul equiments to test

--- equipment_lights_test.py
+++ equipment_lights_test.py
@@ -2,6 +2,10 @@ from equipment_light_bar_display import EquipmentLightBarDisplay
 import time
 
 while True:
-	equip_lightbar = EquipmentLightBarDisplay(5)
-	equip_lightbar.display_equip_status(0,"E0414")
-	time.sleep(1)
+    equip_lightbar = EquipmentLightBarDisplay(5)
+    equip_lightbar.display_equip_status(0,"E0414")
+    equip_lightbar.display_equip_status(1,"E0341")
+    equip_lightbar.display_equip_status(2,"E0333")
+    equip_lightbar.display_equip_status(3,"E0413")
+    equip_lightbar.display_equip_status(4,"E0412")
+    time.sleep(1)
